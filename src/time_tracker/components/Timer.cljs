248effb00707fc1f72b2e5916ace41ab5b846bd0
peterjewicz <peterjewicz@totalwebconnections.com>
2018-12-03 07:45:39 -0600

    Properly format timer output

--- src/time_tracker/components/Timer.cljs
+++ src/time_tracker/components/Timer.cljs
@@ -20,9 +20,33 @@
   (swap! app-state conj {:timerActive false
                          :timerStart false
                          :timerProject false
-                         :timerRunning false})
-  ))
-; READ THIS _______ THINK THE ISSUE IS THAT IT KEEPS SETTING MORE AND MORE INTERVALS
+                         :timerRunning false})))
+
+;TODO if we need this again move it to utilities
+(defn format-time-number
+  "Formats a number to have a leading 0 if < 10"
+  [time]
+  (if (< time 10)
+    (str "0" time)
+    time))
+
+
+(defn format-time
+  "Takes the current time in seconds and formats it better"
+  [seconds]
+  (let [time (atom {:hours 0 :minutes 0 :total seconds})]
+    (if (> (:total @time) 3599) ; handles hours
+      (do
+        (swap! time conj {:hours (Math/floor (/ (:total @time) 3600))})
+        (swap! time conj {:total (mod (:total @time) 3600)})))
+    (if (> (:total @time) 59) ; handles minutes
+      (do
+        (swap! time conj {:minutes (Math/floor (/ (:total @time) 60))})
+        (swap! time conj {:total (mod (:total @time) 60)})))
+    (str (format-time-number (:hours @time))
+          ":" (format-time-number (:minutes @time))
+          ":" (format-time-number (:total @time)))))
+
 (defn render [app-state]
   (let [interval (atom nil)
         currentTime (atom 0)]
@@ -35,5 +59,5 @@
       [:div.Timer {:class (:timerActive @app-state)}
         [:p (:timerProject @app-state)]
         [:p (str "Started At: " (:timerStart @app-state))]
-        [:p (str "Running For: " @currentTime)]
+        [:p (str "Running For: " (format-time @currentTime))]
         [:p {:on-click #(end-timer app-state interval)} "End Timer"]])))
