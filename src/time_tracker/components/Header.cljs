86437d845a94d4ed0b1bcee298c929a4e8f4c804
peterjewicz <peterjewicz@totalwebconnections.com>
2018-12-09 12:35:45 -0600

    add datepicker

--- src/time_tracker/components/Header.cljs
+++ src/time_tracker/components/Header.cljs
@@ -1,9 +1,10 @@
 (ns time-tracker.components.Header
   (:require [reagent.core :as reagent :refer [atom]]
-            [time_tracker.utilities.view_handler :as view_handler]))
+            [time_tracker.utilities.view_handler :as view_handler]
+            ["moment" :as moment]))
 
 (defn render []
   [:div.Header
     [:div [:p {:on-click #(view_handler/change-view {:calendar "active"})} "Calendar"]]
-    [:div [:p "Time Tracker"]]
+    [:div [:p (.format (moment) "dddd, MMMM D")]]
     [:div [:p {:on-click #(view_handler/change-view {:reports "active"})} "Reporting"]]])
--- src/time_tracker/components/Reports.cljs
+++ src/time_tracker/components/Reports.cljs
@@ -1,8 +1,11 @@
 (ns time-tracker.components.Reports
   (:require [reagent.core :as reagent :refer [atom]]
             [time_tracker.utilities.view_handler :as view_handler]
+            [cljs-pikaday.reagent :as pikaday]
             ["localforage" :as localforage]))
 
+(defonce start-date (reagent/atom (js/Date.)))
+(defonce end-date (reagent/atom (js/Date.)))
 
 (defn render []
   (let [project-name (atom "")]
@@ -13,4 +16,7 @@
         [:div [:h3 "Reports"]]
         [:div]]
       [:div.Reports-body
-        [:p "Reporting"]]])))
+        [:p "Reporting"]
+        [pikaday/date-selector {:date-atom start-date}]
+        [pikaday/date-selector {:date-atom end-date}]
+        [:button {:on-click #(js/console.log @start-date)} "Generate"]]])))
