90bf4b759207134baa9a64b625cc1ea05410d20e
peterjewicz <peterjewicz@totalwebconnections.com>
2018-12-13 08:17:50 -0600

    report building

--- src/time_tracker/components/Reports.cljs
+++ src/time_tracker/components/Reports.cljs
@@ -7,14 +7,43 @@
 (defonce start-date (reagent/atom (js/Date.)))
 (defonce end-date (reagent/atom (js/Date.)))
 
-(defn generate-report []
+(defn download-report []
   "Generates the PDF Report Of date/times"
   (let [doc (js/jsPDF.)]
     (.text doc "Hello world!", 10, 10)
     (.save doc  "a4.pdf")))
 
-(defn render []
+(defn check-time-after [time minTime]
+  "Checks whether a date/time provided is greater than another"
+  (if (> time minTime)
+    true
+    false))
+
+(defn check-time-before [time maxTime]
+  "Checks whether a date/time provided is less than another"
+  (if (< time maxTime)
+    true
+    false))
+
+
+(defn generate-report [projects]
+  "Compiles all times between the two dates for all projects"
+  (doseq [date projects]
+    (let [projectName (first (js->clj date))
+          dateValues (second (js->clj date :keywordize-keys true))
+          keys (keys dateValues)]
+          (loop [i 0]
+            (if (= i (count keys))
+              ""
+              (do
+                (print (get dateValues (nth keys i)))
+                (recur (inc i)))))
+    )
+  ))
+
+(defn render [app-state]
   (let [project-name (atom "")]
+  (generate-report (:projectDates @app-state))
   (fn []
     [:div.Reports {:class (:reports @view_handler/active-view)}
       [:div.Reports-header
@@ -28,4 +57,4 @@
         [:br]
         [:label "End Date"]
         [pikaday/date-selector. {:date-atom end-date}]
-        [:button {:on-click #(generate-report)} "Generate"]]])))
+        [:button {:on-click #(generate-report (:projectDates @app-state))} "Generate"]]])))
--- src/time_tracker/utilities/date_formatter.cljs
+++ src/time_tracker/utilities/date_formatter.cljs
@@ -2,17 +2,23 @@
 (ns time_tracker.utilities.date_formatter
   (:require [reagent.core :as reagent :refer [atom]]))
 
-
+(defn generate-time-string [time string]
+  "Generates a string based on time taken knocking off 0 amounts"
+  (if (= 0 time)
+    ""
+    (str time string)))
 
 (defn format-time-taken [start end]
   "Returns a date in the following format `25 Minutes and 32 Seconds`"
   (let [time (atom {:hours 0 :minutes 0 :total (/ (- end start) 1000)})]
   (if (> (:total @time) 3599) ; handles hours
     (do
-      (swap! time conj {:hours (str (Math/floor (/ (:total @time) 3600)) " Hours")})
+      (swap! time conj {:hours (str (Math/floor (/ (:total @time) 3600)))})
       (swap! time conj {:total (mod (:total @time) 3600)})))
   (if (> (:total @time) 59) ; handles minutes
     (do
-      (swap! time conj {:minutes (str (Math/floor (/ (:total @time) 60)) " Minutes")})
+      (swap! time conj {:minutes (str (Math/floor (/ (:total @time) 60)))})
       (swap! time conj {:total (mod (:total @time) 60)})))
-      (str (:hours @time) " and " (:minutes @time) " and " (:total @time) " seconds")))
+      (str (generate-time-string (:hours @time) " Hour(s) ")
+           (generate-time-string (:minutes @time) " Minute(s) ")
+           (generate-time-string (:total @time) " Second(s)"))))
